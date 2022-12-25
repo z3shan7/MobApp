ea773b6a350516056be1520f08b20e8906109763
Shoaib Zeshan <szeeshan707@gmail.com>
2022-12-25 23:58:14 +0530

    building feature functionalities

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ function scrollFunction() {
     }
     // if (document.body.scrollTop > 20) {
 }
+
+//  Tab Functionalites
+
+const container = document.querySelector('.features__container')
+const tabContent = document.querySelectorAll('.features__tab-content')
+const tabBtns = document.querySelectorAll('.features__btn')
+
+
+container.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+
+    if (id) {
+        //  remove active from other buttons
+        tabBtns.forEach((btn) => {
+            btn.classList.remove('active');
+            e.target.classList.add('active')
+        });
+        // hide other content
+
+        tabContent.forEach((content) => {
+            content.classList.remove('active')
+        })
+        const element = document.getElementById(id);
+        element.classList.add('active')
+
+
+    }
+})
